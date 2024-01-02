e0b11764a5087edd123d10af6d4eb14ab7d43dbd
bilalhanifkhatri <bilalhanifkhatri@gmail.com>
2024-01-02 11:20:34 +0500

    complete app

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,17 +1,22 @@
-import { Image, View, Text } from "react-native";
+import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { assets, COLORS, SHADOWS, SIZES } from "../constants";
+import { View, Image } from "react-native";
+
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
+import { RectButton, CircleButton } from "./Button";
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
+
   return (
     <View
       style={{
-        backgroundColor: COLORS?.white,
-        borderRadius: SIZES?.font,
-        marginBottom: SIZES?.extraLarge,
-        margin: SIZES?.base,
-        ...SHADOWS?.dark,
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
       }}
     >
       <View
@@ -21,17 +26,45 @@ const NFTCard = ({ data }) => {
         }}
       >
         <Image
+          source={data.image}
+          resizeMode="cover"
           style={{
             width: "100%",
             height: "100%",
-            resizeMode: "cover",
-            borderTopLeftRadius: SIZES?.font,
-            borderTopRightRadius: SIZES?.font,
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
           }}
-          source={{ uri: data.image }}
         />
+
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>NFTCard</Text>
+
+      <SubInfo />
+
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- screens/Home.js
+++ screens/Home.js
@@ -1,49 +1,55 @@
-import { Text, View } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
+import { View, SafeAreaView, FlatList } from "react-native";
+
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 import { COLORS, NFTData } from "../constants";
-import { FocusedStatusBar, HomeHeader, NFTCard } from "../components";
-import { FlatList } from "react-native";
+
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <FocusedStatusBar background={COLORS?.primary} />
+      <FocusedStatusBar backgroundColor={COLORS.primary} />
       <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={nftData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+          />
+        </View>
+
         <View
           style={{
-            zIndex: 0,
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
           }}
         >
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            data={NFTData}
-            renderItem={({ item }) => <NFTCard data={item} />}
-            keyExtractor={(item) => item.id.toString()}
-            ListHeaderComponent={<HomeHeader />}
-          />
           <View
-            style={{
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              right: 0,
-              top: 0,
-              zIndex: -1,
-            }}
-          >
-            <View
-              style={{
-                backgroundColor: COLORS?.primary,
-                height: 300,
-              }}
-            ></View>
-            <View
-              style={{
-                backgroundColor: COLORS?.white,
-                flex: 1,
-              }}
-            ></View>
-          </View>
+            style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
         </View>
       </View>
     </SafeAreaView>
